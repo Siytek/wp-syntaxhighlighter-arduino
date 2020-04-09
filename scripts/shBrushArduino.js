d4b29c3c8bdd13635bdba303d323bea0b0a8dba9
Simon <siy@btinternet.com>
2020-04-09 12:14:31 +0100

    Update shBrushArduino.js
    Now compatible with SyntaxHighlighter Evolved

--- scripts/shBrushArduino.js
+++ scripts/shBrushArduino.js
@@ -9,24 +9,33 @@
  * http://alexgorbatchev.com/
  *
  * This brush file is released to the public domain. Do with it as you see fit.
- * However, I'd appreciate it if you continued to credit me and leave in the 
+ * However, I'd appreciate it if you continued to credit me and leave in the
  * reference to my website (http://upnotnorth.net).
+ *
+ * Brush updated for SyntaxHighlighter Evolved by https://siytek.com
  */
-SyntaxHighlighter.brushes.arduino = function()
+;(function()
 {
+	// CommonJS
+	SyntaxHighlighter = SyntaxHighlighter || (typeof require !== 'undefined'? require('shCore').SyntaxHighlighter : null);
+
+	function Brush()
+	{
+
 	var datatypes =	'boolean char byte int long float double void unsigned volatile word string static const';
 
 	var keywords =	'setup loop if else for switch case default while do break continue return';
-					
+
 	var functions =	'pinMode digitalWrite digitalRead analogRead analogWrite shiftOut pulseIn ' +
 			'millis micros delay delayMicroseconds min max abs constrain ' +
-			'map pow sq sqrt sin cos tan randomSeed random ' + 
+			'map pow sq sqrt sin cos tan randomSeed random ' +
 			'sizeof lowByte highByte bitRead bitWrite bitSet bitClear bit tone noTone' +
 			'attachInterrupt detachInterrupt interrupts noInterrupts ' +
-			'Serial\\.begin Serial\\.available Serial\\.read Serial\\.flush ' + 
+			'Serial\\.begin Serial\\.available Serial\\.read Serial\\.flush ' +
 			'Serial\\.print Serial\\.println Serial\\.write ';
-					
+
 	var constants = 'HIGH LOW INPUT OUTPUT true false CHANGE RISING FALLING';
+
 
 	this.regexList = [
 		{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' }			// one line comments
@@ -43,9 +52,15 @@ SyntaxHighlighter.brushes.arduino = function()
 		,{ regex: /\b0x[0-9A-Fa-f]+[uU]?[lL]?\b/gm,					css: 'constants'} 	// numeric constants (hexidecimal)
 		,{ regex: /\bB[01]{1,8}\b/gm,								css: 'constants'} 	// numeric constants (binary)
 		,{ regex: /\+|\-|\*|\/|\%|!|\||\&amp;|=|\?|\^|~/gm, 			css: 'plain bold' }		// operators
-       //,{ regex: /\b(?:\w+?(?=\(.*?\)\W))/gm,							css: 'plain italic'}		// Other functions/macros (i.e. user-defined)        
+       //,{ regex: /\b(?:\w+?(?=\(.*?\)\W))/gm,							css: 'plain italic'}		// Other functions/macros (i.e. user-defined)
 		];
-};
+	};
 
-SyntaxHighlighter.brushes.arduino.prototype	= new SyntaxHighlighter.Highlighter();
-SyntaxHighlighter.brushes.arduino.aliases	= ['arduino', 'arduinolite'];
+	Brush.prototype	= new SyntaxHighlighter.Highlighter();
+	Brush.aliases	= ['arduino', 'arduinolite'];
+
+	SyntaxHighlighter.brushes.Arduino = Brush;
+
+	// CommonJS
+	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
